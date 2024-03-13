6e2bca51aa2a9c12d66824880c28d4afb1ab4a71
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-13 19:51:42 -0400

    1

--- Animal-webpage/main.js
+++ Animal-webpage/main.js
@@ -1,11 +1,29 @@
 
+
+var pageCounter = 1;
+
+var animalContainer = document.getElementById('animal-info');
 var btn = document.getElementById('btn')
 btn.addEventListener('click', function(){
 
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     ourRequest.onload = function(){
         var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
     };
     ourRequest.send();
+    pageCounter++;
+    if(pageCounter > 3){
+        btn.classList.add('hide-me');
+    }
 }); 
+
+function renderHTML(data){
+    htmlString = "";
+
+    for(i = 0; i < data.length; i++){
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " .</p>"; 
+    }
+    animalContainer.insertAdjacentHTML('beforeend', htmlString)
+}

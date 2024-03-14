6b33da7417d93c1665aac801197aebd8f56a387f
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-13 20:03:22 -0400

    finalized code

--- Animal-webpage/main.js
+++ Animal-webpage/main.js
@@ -20,10 +20,29 @@ btn.addEventListener('click', function(){
 }); 
 
 function renderHTML(data){
-    htmlString = "";
+    var htmlString = "";
 
-    for(i = 0; i < data.length; i++){
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " .</p>"; 
+    for(var i = 0; i < data.length; i++){
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+        
+        for (var ii = 0; ii < data[i].foods.likes.length; ii++){
+            if (ii == 0){
+                htmlString += data[i].foods.likes[ii];
+            }else{
+                htmlString += " and " + data[i].foods.likes[ii];
+            }
+        } 
+
+        htmlString += ' and dislikes ';
+        
+        for (var ii = 0; ii < data[i].foods.dislikes.length; ii++){
+            if (ii == 0){
+                htmlString += data[i].foods.dislikes[ii];
+            }else{
+                htmlString += " and " + data[i].foods.dislikes[ii];
+            }
+        }
+        htmlString += '.</p>'
     }
-    animalContainer.insertAdjacentHTML('beforeend', htmlString)
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
